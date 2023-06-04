047699fff280a82880b211e4eac3e9d676e7d76d
Bartoszhumel <79085432+Bartoszhumel@users.noreply.github.com>
2023-06-04 14:27:05 +0200

    adding stripe payment

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,6 +3,9 @@ import {CartItem} from "../components/CartItem.tsx";
 import {ShopContext} from "../context/shop-context.tsx";
 import "../components/CartItem.css";
 import {useNavigate} from "react-router-dom";
+import {loadStripe} from '@stripe/stripe-js';
+const stripePromise = loadStripe('pk_live_51N9TnYGPv8HgnGdPJ64IrP39AuvdOFn0PZ8ias5mg9UpqfSZDIz1maVmpIMwkKeAheNJGL51vVWUFpq2xlYcjkBS00pTbpukIU')
+
 export const Cart = () => {
     const {cartItems} = useContext(ShopContext);
 
@@ -35,6 +38,16 @@ export const Cart = () => {
         return total;
     }
     const totalCartAmount = getTotalCartAmount()
+    async function doPayment() {
+       const stripeResp = await fetch('http://localhost:4000/payment?price='+totalCartAmount);
+       const {id: sessionId} = await stripeResp.json();
+
+         const stripe = await stripePromise;
+            const {error} = await stripe.redirectToCheckout({
+                sessionId,
+            });
+            console.log(error);
+    }
 
     return (
         <div className="cart">
@@ -53,7 +66,7 @@ export const Cart = () => {
             <div className="Checkout">
                 <p>Suma: {totalCartAmount}zł</p>
                 <button onClick={()=> nav("/menu")}>Kontynuj zakupy </button>
-                <button>Do kasy</button>
+                <button onClick={doPayment}>Do kasy</button>
             </div>
                 ):(
                     <h1>Brak produktów w koszyku</h1>
